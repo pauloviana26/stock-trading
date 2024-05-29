939b747ade10bb538101d1f54f2fb25199e0a640
Paulo Viana <pauloc.fviana@hotmail.com>
2024-05-29 11:08:54 +0100

    spring webflux configured

---
+++ src/main/java/com/pluralsight/springwebflux6/stocktrading/model/Stock.java
@@ -0,0 +1,14 @@
+package com.pluralsight.springwebflux6.stocktrading.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Stock {
+    private String name;
+}
